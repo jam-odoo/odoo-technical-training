46e7550b50858ccee545a135b62da5ff3df1fe48
jam-odoo <jam@odoo.com>
2016-09-28 16:14:34 -0700

    Day 3 EOF Code

--- v9_sep_19/openacademy/models/openacademy.py
+++ v9_sep_19/openacademy/models/openacademy.py
@@ -37,7 +37,21 @@ class OpenacademyCourse(models.Model):
     active = fields.Boolean(stirng="Archived", default=True)
     notes = fields.Html(string="Notes")
     code = fields.Char(string="Code", size=16)
+    session_ids = fields.One2many(comodel_name="openacademy.session", inverse_name="course_id", string="Sesions")
+    session_count = fields.Integer(compute="_compute_session_count", store=True, string="Session Count")
 
+    @api.multi
+    @api.depends("session_ids")
+    def _compute_session_count(self):
+        for record in self:
+            record.session_count = len(record.session_ids)
+
+    @api.multi
+    def name_get(self):
+        vals = []
+        for record in self:
+            vals.append((record.id, "[{}] {}".format(record.code, record.name)))
+        return vals
 
 class openacademy_session(models.Model):
     """Open Academy Session"""
@@ -49,8 +63,9 @@ class openacademy_session(models.Model):
     sequence = fields.Integer(string="Sequence", default=10)
     active = fields.Boolean(stirng="Archived", default=True)
     code = fields.Char(string="Code", size=16)
-    max_seat = fields.Integer(string="Maximum Seats", required=True,\
+    max_seat = fields.Integer(string="Maximum Avaiable Seats", required=True,\
                                  default=10, index=True)
+    min_seat = fields.Integer(string="Minimum Required Seats", required=True, index=True)
     duration_days = fields.Float(string="Duration(days)", digits=(6,3),\
                                  required=True, default=1)
     start_date = fields.Datetime(string="Start Date")
@@ -63,11 +78,21 @@ class openacademy_session(models.Model):
     instructor_id = fields.Many2one(comodel_name="res.partner", string="Instructor", domain="[('supplier', '=', True)]")
     course_id = fields.Many2one(comodel_name="openacademy.course", string="Course", required=True)
     tag_ids = fields.Many2many(comodel_name="openacademy.tags", relation="rel_session_tags", column1="session_id", column2="tag_id", string="Tags")
+    attendee_ids = fields.One2many(comodel_name="openacademy.attendee", inverse_name="session_id", string="Attendees")
+    total_invited = fields.Integer(compute="count_total_invited", string="Total Invited")
+    total_attneding = fields.Integer(compute="count_total_invited", string="Total Attending")
 
 
     _sql_constraints = [
         ("session_code_unique", "unique (code)", _("The code must ne unique !")),
     ]
+
+    @api.multi
+    @api.depends("attendee_ids")
+    def count_total_invited(self):
+        for record in self:
+            self.total_invited = sum([att.count for att in record.attendee_ids ])
+            self.total_attneding = sum([att.count for att in record.attendee_ids if att.state == "going"])
 
     @api.constrains("start_date", "end_date")
     def date_validation(self):
@@ -97,3 +122,24 @@ class openacademy_session(models.Model):
         self.end_date = end_date
 
 
+class OpenacademyAttendee(models.Model):
+    """OpenAcademy Attendee"""
+
+    _name = "openacademy.attendee"
+
+    name = fields.Char(string="Attendee Name", required=True)
+    partner_id = fields.Many2one(comodel_name="res.partner", string="Partner")
+    email = fields.Char(string="Email")
+    count = fields.Integer(string="Invited Individual")
+    session_id = fields.Many2one(comodel_name="openacademy.session", string="Session")
+    state = fields.Selection(string="Status", selection=[("invite", "Invited"), ("going", "Going to Attned"), ("not", "Not Attending"), ("unsure", "Unsure")], default="invite")
+
+    @api.onchange("partner_id")
+    def oc_partner(self):
+        if self.partner_id:
+            self.write({
+                "email": self.partner_id.email,
+                "name": self.partner_id.name,
+            })
+            # self.email = self.partner_id.email
+            # self.name = self.partner_id.name
