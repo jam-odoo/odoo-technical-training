b0bfebe4907ce92c0d5d395ba76d2558deb9b50f
jam-odoo <jam@odoo.com>
2017-04-10 15:48:22 -0700

    [ADD] 04/17 Day 1 App

---
+++ v10_april_17/openacademy/__manifest__.py
@@ -0,0 +1,25 @@
+{    
+    "name": "OpenAcademy Managment",
+    "description": """
+OpenAcademy Managment
+===============================
+* Sessions Managment
+* Course Managment
+* etc...
+""",
+    "summary": "Sessions,Courses",
+    "license": "LGPL-3",
+    "version": "1.0",
+    "category": "Extra Addons",
+    "sequence": 0,
+    "website": "https://www.odoo.com",
+    "author": "Odoo, Inc",
+    "depends": ["base"],
+    "data":[
+    ],
+    "demo": [
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
---
+++ v10_april_17/openacademy/models/openacademy_course.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models
+
+class OpenAcademyCourse(models.Model):
+    """OpenAcademy Course Managment"""
+
+    _name = "openacademy.course"
+
+    
