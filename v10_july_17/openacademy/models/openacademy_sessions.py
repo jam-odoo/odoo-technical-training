61c011caa51c7e8ded0fc5d5ae93684073a8f9b4
Jigar Amin (Odoo) <jam@odoo.com>
2017-07-12 14:32:34 -0700

    [ADD] Added New model and demo

--- v10_july_17/openacademy/models/openacademy_sessions.py
+++ v10_july_17/openacademy/models/openacademy_sessions.py
@@ -4,6 +4,13 @@ from odoo import models
 from odoo import fields
 
 
+class OpenAcademyTags(models.Model):
+
+    _name = "openacademy.tags"
+
+    name = fields.Char(string="Session Name", required=True, index=True)
+    color = fields.Integer(string="Color")
+
 class OpenacademySession(models.Model):
     """Openacademy Sesssion Model"""
 
@@ -35,4 +42,26 @@ class OpenacademySession(models.Model):
     rem_seat_per = fields.Float(string="Remaining Seats(%)")
     admin_email = fields.Char(string="Admin Email")
     session_url = fields.Char(string="Published URL")
-    
+    instructor_id = fields.Many2one(comodel_name="res.partner", string="Instructor", ondelete="restrict", required=True)
+    user_id = fields.Many2one(comodel_name="res.users", string="Resnposible", required=True)
+    attendee_ids = fields.Many2many(comodel_name="res.partner", relation="rel_session_partner_m2m", column1="session_id", column2="parnter_id", string="Partner Attendees")
+    course_id = fields.Many2one(comodel_name="openacademy.course", string="Course", required=True)
+    tag_ids = fields.Many2many(comodel_name="openacademy.tags", string="Tags")
+
+class OpenacademyCourse(models.Model):
+    """Openacademy Sesssion Model"""
+
+    _name  = "openacademy.course"
+
+    name = fields.Char(string="Course Name")
+    code = fields.Char(string="Code", size=32)
+    user_id = fields.Many2one(comodel_name="res.users", string="Resnposible", required=True)
+    course_contain = fields.Html(string="Course Contains") 
+    session_ids = fields.One2many(comodel_name="openacademy.session", inverse_name="course_id", string="Sessions")
+
+
+class Partner(models.Model):
+
+    _inherit = "res.partner"
+
+    session_ids = fields.Many2many(comodel_name="openacademy.session", relation="rel_session_partner_m2m", column1="parnter_id", column2="session_id", string="Sesssions")
