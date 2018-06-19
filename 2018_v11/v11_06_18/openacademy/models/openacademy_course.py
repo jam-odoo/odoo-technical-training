ea1ff581f6332b9832d07a0e94f9691b4ceca20d
Jigar A <jam@odoo.com>
2018-06-19 16:02:24 -0700

    [ADD]D2 More fields and demo, icon

--- 2018_v11/v11_06_18/openacademy/models/openacademy_course.py
+++ 2018_v11/v11_06_18/openacademy/models/openacademy_course.py
@@ -8,7 +8,8 @@ class OpenAcademyCouse(models.Model):
 
     name = fields.Char(string='Title', required=True, index=True,
                         help='Couse title goes here (e.g. Odoo Technical Training)')
-    notes = fields.Html(string="Notes")
     active = fields.Boolean(string="Active", default=True)
+    session_count = fields.Integer("# Sessoin")
+    notes = fields.Html(string="Notes")
     banner = fields.Binary(string='Banner')
     banner_fname = fields.Char(string="Banner File Name")
--- 2018_v11/v11_06_18/openacademy/models/openacademy_sessions.py
+++ 2018_v11/v11_06_18/openacademy/models/openacademy_sessions.py
@@ -7,7 +7,7 @@ class OpenAcademySession(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(string='Title', required=True, index=True,
-                        help='Session title goes here... e.g. Odoo Technical Training June 2018',
+                        help='Title goes here... e.g. Odoo Technical Training June 2018',
                         size=200)
     start_date = fields.Datetime(string='Start Date', required=True)
     duration = fields.Float(string="Duration (Days)", digits=(5,2), default=1)
@@ -17,9 +17,12 @@ class OpenAcademySession(models.Model):
     total_seat = fields.Integer(string="Total Seats", default=10)
     min_seat = fields.Integer(string="Minimum Required Registrations")
     can_overbook = fields.Boolean(string="Can be Overbooked")
+    remmain_seat_per = fields.Float(string="Remaing Seats(%)", digits=(5,2), default=0.0)
+    booked_seats = fields.Integer(string="Booked Seats")
     all_day = fields.Selection(selection=[('yes', 'Yes'),('no', 'No')], string="Allday Event")
     state = fields.Selection(selection=[('new', 'New'),
                                       ('approve', 'Approved'),
                                       ('open', 'Open'),
                                       ('cancel', 'Cancel'),
                                       ('done', 'Done')], string="Status", default="new")
+    image = fields.Binary(string="Image")
