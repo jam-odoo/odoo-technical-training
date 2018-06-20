9084f58c1a599b7b4f372cba1147132c75a7e176
Jigar A <jam@odoo.com>
2018-06-20 14:32:33 -0700

    [ADD] D3.5 API, OC, Compute, button

--- 2018_v11/v11_06_18/openacademy/models/openacademy_course.py
+++ 2018_v11/v11_06_18/openacademy/models/openacademy_course.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api, _
 
 class OpenAcademyCouse(models.Model):
 
@@ -9,7 +9,21 @@ class OpenAcademyCouse(models.Model):
     name = fields.Char(string='Title', required=True, index=True,
                         help='Couse title goes here (e.g. Odoo Technical Training)')
     active = fields.Boolean(string="Active", default=True)
-    session_count = fields.Integer("# Sessoin")
+    user_id = fields.Many2one(comodel_name='res.users', string='Responsible')
+    session_ids = fields.One2many(comodel_name="openacademy.session", inverse_name="course_id", string="Sessions")
+    session_count = fields.Integer(string="# Sessions", compute="_get_session_count")
     notes = fields.Html(string="Notes")
     banner = fields.Binary(string='Banner')
     banner_fname = fields.Char(string="Banner File Name")
+
+    @api.multi
+    @api.depends('session_ids')
+    def _get_session_count(self):
+    	for course in self:
+    		course.session_count =  len(course.session_ids)
+
+    def open_sessions(self):
+    	action_id = self.env.ref("openacademy.action_view_openacademy_sessions")
+    	action_data = action_id.read()[0]
+    	action_data.update({'domain': [('course_id', '=', self.id)]})
+    	return action_data
--- 2018_v11/v11_06_18/openacademy/models/openacademy_sessions.py
+++ 2018_v11/v11_06_18/openacademy/models/openacademy_sessions.py
@@ -1,15 +1,38 @@
 # -*- encoding: utf-8 -*-
 
-from odoo import models, fields
+import logging
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+class OpenAcademyTags(models.Model):
+
+    _name = 'openacademy.tags'
+
+    name = fields.Char(string='Tag', required=True, index=True)
+    color = fields.Integer(string="Color Index")
 
 class OpenAcademySession(models.Model):
 
     _name = 'openacademy.session'
 
+    def _get_default_user(self):
+        return self.env.user.id
+
+
     name = fields.Char(string='Title', required=True, index=True,
                         help='Title goes here... e.g. Odoo Technical Training June 2018',
                         size=200)
-    start_date = fields.Datetime(string='Start Date', required=True)
+    course_id = fields.Many2one(comodel_name="openacademy.course", required=True)
+    user_id = fields.Many2one(comodel_name='res.users', string='Responsible', default=_get_default_user)
+    user_email = fields.Char(related="user_id.email", string="Email")
+    code = fields.Char(string="Code", szie=64)
+    start_date = fields.Datetime(string='Start Date', required=True, default=fields.Datetime.now())
     duration = fields.Float(string="Duration (Days)", digits=(5,2), default=1)
     end_date = fields.Datetime(string='End Date')
     notes = fields.Html(string="Description")
@@ -26,3 +49,50 @@ class OpenAcademySession(models.Model):
                                       ('cancel', 'Cancel'),
                                       ('done', 'Done')], string="Status", default="new")
     image = fields.Binary(string="Image")
+    tag_ids = fields.Many2many(comodel_name="openacademy.tags", relation="rel_openacademy_session_tags",
+                                column1="sessoin_id", column2="tag_id",string="Tags")
+
+
+    _sql_constraints = [
+        ('unique_sessoin_code', 'UNIQUE (code)','The code must be unique !'),
+    ]
+
+    @api.constrains('start_date', 'end_date')
+    @api.multi
+    def _check_dates(self):
+        for record in self:
+            if record.start_date and record.end_date and record.start_date > record.end_date:
+                _logger.warning("Sesssion '{}' ({}) was attmepted to save with wrong dates by user {}.".format(record.name, record, self.env.user.id))
+                raise UserError(_("Start date can not be in future of end date."))
+
+    @api.onchange("course_id")
+    def onchange_course_id(self):
+        #self.ensure_one()
+        if self.course_id:
+            self.image = self.course_id.banner
+
+    @api.onchange("start_date", "duration")
+    def onchange_dates(self):
+        #self.ensure_one()
+        if self.start_date:
+            self.end_date = datetime.strptime(self.start_date, "%Y-%m-%d %H:%M:%S") + relativedelta(days=self.duration)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('code'):
+            vals.update({'code': vals['code'].upper()})
+        res = super(OpenAcademySession, self).create(vals)
+
+        return res
+
+    @api.multi
+    def write(self, vals):
+        if vals.get('code'):
+            vals.update({'code': vals['code'].upper()})
+        return super(OpenAcademySession, self).write(vals)
+
+    @api.multi
+    def unlink(self):
+        #raise ValidationError(_("Session can not be deledted, please archive the record"))
+        self.active = False
+        return True
