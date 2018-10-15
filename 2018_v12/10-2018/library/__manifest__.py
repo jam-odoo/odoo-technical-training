0923e50bd26a8086ce8e8d168657e850566cc660
Jigar A <jam@odoo.com>
2018-10-15 16:05:50 -0700

    [ADD] Added v12 lib module

---
+++ 2018_v12/10-2018/library/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+	'name': 'Library Managment',
+	'version': '0.1', 
+	'website': 'https://www.odoo.com',
+	'summary': 'Library, Books, Custoemr Managment',
+	'sequence': 2,
+	'category': 'Library',
+	'author': 'Odoo Inc',
+	'description': '''reText''',
+	'depends': ['base'],
+	'data': [],
+	'demo': [],
+	'application': True,
+	'auto_install': False,
+	'installable': True,
+}
